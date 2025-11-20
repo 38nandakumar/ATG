dcc5844eddc2a3889af21067eca9d914d1c874c1
38nandakumar <38nandakumar@gmail.com>
2025-11-20 20:52:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,40 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, Collection, SlashCommandBuilder, Events } = require("discord.js");
 const { DisTube } = require("distube");
 const { YtDlpPlugin } = require("@distube/yt-dlp");
+const { token } = require("./config.json");
 
+// Create client
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages
+    ],
+    partials: [Partials.Channel]
 });
 
-const distube = new DisTube(client, {
-  plugins: [new YtDlpPlugin()]
-});
+// Slash commands collection
+client.commands = new Collection();
 
-client.on("ready", () => {
-  console.log(`Bot Logged in as ${client.user.tag}`);
-});
-
-client.on("messageCreate", async (message) => {
-  if (!message.guild) return;
-
-  const prefix = "!";
-
-  if (!message.content.startsWith(prefix)) return;
-
-  const args = message.content.slice(prefix.length).split(" ");
-  const cmd = args.shift();
-
-  if (cmd === "play") {
-    if (!args[0]) return message.reply("Song URL or name kudu!");
-    distube.play(message.member.voice.channel, args.join(" "), {
-      textChannel: message.channel,
-      member: message.member
-    });
-  }
-
-  if (cmd === "stop") {
-    distube.stop(message);
-    message.channel.send("Music stopped!");
-  }
-
-  if (cmd === "skip") {
-    distube.skip(message);
-    message.channel.send("Skipped!");
-  }
-});
-
-// DisTube events
-distube
-  .on("playSong", (queue, song) =>
-    queue.textChannel.send(`Playing: **${song.name}**`)
-  )
-  .on("addSong", (queue, song) =>
-    queue.textChannel.send(`Added: **${song.name}**`)
-  );
-
-client.login(process.env.TOKEN);
+// Register slash commands
+const commands = [
+    new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Play music from YouTube")
+        .addStringOption(option =>
+            option.setName("song")
+                .setDescription("Song name or YouTube link")
+                .setRequired(true)
+        ),
+    new SlashCommandBuilder()
+        .setName("stop")
+        .setDescription("Stop the music"),
+    new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription("Skip current song"),
+    new SlashCommandBuilder()
+        .setName("pause")
+        .setDescription("Pause music"),
+    new SlashCommandBuilder()
+        .setName("resum
 
