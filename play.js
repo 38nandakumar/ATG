fc8130271cbc76a0da825b28305eadc84ce1145b
38nandakumar <38nandakumar@gmail.com>
2025-11-20 19:33:00 +0530

    Create play.js

---
+++ play.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('play')
+        .setDescription('Play a song from YouTube')
+        .addStringOption(option =>
+            option.setName('song')
+                .setDescription('Song name or URL')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const song = interaction.options.getString('song');
+        await interaction.reply(`🎵 Playing: ${song}`);
+    }
+};
