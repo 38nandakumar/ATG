043b0eb16366d2b8a14deaceb03f589d796ac4d6
38nandakumar <38nandakumar@gmail.com>
2025-11-20 21:33:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,9 +47,3 @@ client.once('ready', () => {
 // Bot login using GitHub Secrets
 client.login(process.env.BOT_TOKEN);
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-}
